53ffb91dad8474d96f0fdd660e02a2ca86c9369e
Dmitry Zheltukhin <Dmitry.Zheltukhin@enghouse.com>
2018-05-22 15:57:06 +0300

    sshd test

--- src/mina/sshd/SshdShell.java
+++ src/mina/sshd/SshdShell.java
@@ -4,6 +4,7 @@ import jdk.nashorn.internal.ir.Terminal;
 import org.apache.sshd.client.SshClient;
 import org.apache.sshd.client.channel.ChannelExec;
 import org.apache.sshd.client.channel.ChannelShell;
+import org.apache.sshd.client.channel.ClientChannel;
 import org.apache.sshd.client.channel.ClientChannelEvent;
 import org.apache.sshd.client.channel.PtyCapableChannelSession;
 import org.apache.sshd.client.config.hosts.HostConfigEntryResolver;
@@ -11,9 +12,12 @@ import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
 import org.apache.sshd.client.session.ClientSession;
 import org.apache.sshd.client.simple.SimpleClient;
 import org.apache.sshd.common.keyprovider.KeyPairProvider;
+import org.apache.sshd.common.util.io.NoCloseInputStream;
+import org.apache.sshd.common.util.io.NoCloseOutputStream;
 import util.StreamPrinter;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -41,6 +45,37 @@ public class SshdShell {
         }
     }
 
+    public  String executeCommand(final String host, final String username, final String password,
+                                  final String command) throws IOException {
+
+        client.start();
+
+        try (ClientSession session = simple.sessionLogin(host, 22, username, password)) {
+            System.out.println("Session is : " + session.isAuthenticated());
+
+            ChannelShell channel = session.createShellChannel();
+
+            channel.setIn(new NoCloseInputStream(new ByteArrayInputStream(command.getBytes())));
+            ByteArrayOutputStream out=new ByteArrayOutputStream();
+            channel.setOut(new NoCloseOutputStream(out));
+            channel.setErr(new NoCloseOutputStream(out));
+            channel.open();
+            //channel.waitFor(ClientChannel.CLOSED, 0);
+
+            String[] lines=new String(out.toByteArray()).split("\r\n");
+            StringBuffer result=new StringBuffer();
+            for(int i=21;i<lines.length-1;i++) {
+                result.append(lines[i]).append("\n");
+            }
+
+            // visual output of Fuse/Karaf interactions
+            if (!"".equals(result.toString().trim()))
+                System.out.println("out >> "+result.toString().trim());
+//        System.out.println("out >> "+result.toString().trim());
+            return result.toString();
+        }
+    }
+
     public String executeTelnetCommand(final String host, final String username, final String password,
                                       final String command) throws IOException, InterruptedException {
         client.start();
@@ -49,6 +84,7 @@ public class SshdShell {
             System.out.println("Session is : " + session.isAuthenticated());
 
             ChannelShell shell = session.createShellChannel();
+            shell.setUsePty(true);
             shell.setPtyType("vt102");
 
             shell.setIn(new ByteArrayInputStream(new byte[0]));
