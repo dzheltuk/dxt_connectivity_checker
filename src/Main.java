01c983e16859b6fb7302553407915ef76588bfbb
Dmitry Zheltukhin <Dmitry.Zheltukhin@enghouse.com>
2018-05-22 13:27:42 +0300

    main for sshj and sshd

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import mina.sshd.SshdShell;
 import sshj.SshjShell;
 
 import java.io.IOException;
@@ -11,9 +12,30 @@ public class Main {
     private static final String COMMAND_EXIT = "ZZZ;\n\r";
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        testSshj();
+        testSshd();
+    }
+    private static void testSshd() throws InterruptedException, IOException {
+        SshdShell sshdShell = new SshdShell();
+        System.out.println("sshd executing...");
+        try {
+            sshdShell.init();
+            sshdShell.executeTelnetCommand(HOST_NAME, USERNAME, PASSWORD, COMMAND);
+        } catch (Exception e) {
+            System.out.println("Unable to execute " + e.getMessage());
+        } finally {
+            Thread.sleep(100);
+            sshdShell.executeTelnetCommand(HOST_NAME, USERNAME, PASSWORD, COMMAND_EXIT);
+            try {
+                sshdShell.close();
+            } catch (Exception e) {
+                System.out.println("Unable to close sshd channel " + e.getMessage());
+            }
+        }
+    }
 
+    private static void testSshj() throws InterruptedException, IOException {
         SshjShell sshjShell = new SshjShell();
-
         try {
             System.out.println(sshjShell.executeTelnetCommand(HOST_NAME, USERNAME, PASSWORD, COMMAND));
         } catch (Exception e) {
---
+++ src/mina/sshd/SshdShell.java
@@ -0,0 +1,68 @@
+package mina.sshd;
+
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ChannelExec;
+import org.apache.sshd.client.config.hosts.HostConfigEntryResolver;
+import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.client.simple.SimpleClient;
+import org.apache.sshd.common.keyprovider.KeyPairProvider;
+import org.apache.sshd.server.SshServer;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.apache.sshd.common.util.*;
+import org.apache.sshd.server.auth.pubkey.AcceptAllPublickeyAuthenticator;
+
+public class SshdShell {
+    private static final long CONNECT_TIMEOUT = TimeUnit.SECONDS.toMillis(5L);
+    private static final long AUTH_TIMEOUT = TimeUnit.SECONDS.toMillis(7L);
+
+    private SshServer sshd;
+    private SshClient client;
+    private SimpleClient simple;
+
+    public void init() {
+        client = setupTestClient(getClass());
+        simple = SshClient.wrapAsSimpleClient(client);
+        simple.setConnectTimeout(CONNECT_TIMEOUT);
+        simple.setAuthenticationTimeout(AUTH_TIMEOUT);
+    }
+
+    public void close() throws Exception {
+        if (sshd != null) {
+            sshd.stop(true);
+        }
+        if (simple != null) {
+            simple.close();
+        }
+        if (client != null) {
+            client.stop();
+        }
+    }
+
+    public void executeTelnetCommand(final String host, final String username, final String password,
+                                      final String command) throws IOException {
+        client.start();
+
+        try (ClientSession session = simple.sessionLogin(host, 22, username, password)) {
+            //assertEquals("Mismatched session username", getCurrentTestName(), session.getUsername());
+            System.out.println("Session is : " + session.isAuthenticated());
+
+            ChannelExec channel = session.createExecChannel(command);
+            channel.setPtyType("vt102");
+
+            channel.setIn(new ByteArrayInputStream(new byte[0]));
+            channel.setAgentForwarding(true);
+        }
+    }
+
+    public static SshClient setupTestClient(Class<?> anchor) {
+        SshClient client = SshClient.setUpDefaultClient();
+        client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
+        client.setHostConfigEntryResolver(HostConfigEntryResolver.EMPTY);
+        client.setKeyPairProvider(KeyPairProvider.EMPTY_KEYPAIR_PROVIDER);
+        return client;
+    }
+}
